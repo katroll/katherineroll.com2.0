ac80c4382607a08382e9e0993833cedde7ea57c4
katroll <kroll256@GMAIL.COM>
2022-03-14 18:36:34 -0400

    width fix on project description

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -36,7 +36,7 @@ function ProjectContainer() {
                     <p className="lg:text-2xl text-5xl text-center font-bold text-gray-900">My Carbon Footprint</p>
                     <div className="lg:p-y-2 pt-3">
                         <p className="text-3xl lg:text-base text-center font-semibold text-gray-800 mb-4">Group project completed at The Flatiron School</p>
-                        <p className="text-4xl lg:text-base text-gray-900 w-3/4 text-justify w-full">My Carbon Footprint was created to give you a fun and easy way to get a snapshot of your personal carbon footprint. The app takes a look at some of the most common carbon emitting activities and provides you the ability to fill in your personal details and receive an estimate of carbon emissions based on data provided by the Carbon Interface REST API. </p>
+                        <p className="text-4xl lg:text-base text-gray-900 text-justify w-full">My Carbon Footprint was created to give you a fun and easy way to get a snapshot of your personal carbon footprint. The app takes a look at some of the most common carbon emitting activities and provides you the ability to fill in your personal details and receive an estimate of carbon emissions based on data provided by the Carbon Interface REST API. </p>
                         <div className="flex flex-col lg:flex-row mt-3 items-center lg:justify-center">
                             <p className="text-4xl lg:text-base font-semibold text-gray-800">Technoligies:</p>
                             <p className="text-4xl lg:text-base text-gray-800 pl-5">React.js, Ruby on Rails</p>
@@ -47,7 +47,7 @@ function ProjectContainer() {
                     <p className="lg:text-2xl text-5xl text-center font-bold text-gray-900">Three.js Car Driving App</p>
                     <div className="lg:p-y-2 pt-3">
                         <p className="text-3xl lg:text-base text-center font-semibold text-gray-800 mb-4">Project created while learning three.js</p>
-                        <p className="text-4xl lg:text-base text-gray-900 w-3/4 text-justify w-full">I am interested in three.js and continuing to learn and add to this project. The demo is availabe <NavLink to="/threeproject" className="font-semibold cursor-pointer text-slate-300">here</NavLink>, where you can use the arrow keys on the keyboard to drive around in the world I am slowly expanding on and updating as I learn new Three.js skills.</p>
+                        <p className="text-4xl lg:text-base text-gray-900 text-justify w-full">I am interested in three.js and continuing to learn and add to this project. The demo is availabe <NavLink to="/threeproject" className="font-semibold cursor-pointer text-slate-300">here</NavLink>, where you can use the arrow keys on the keyboard to drive around in the world I am slowly expanding on and updating as I learn new Three.js skills.</p>
                         <div className="flex flex-col lg:flex-row mt-3 items-center lg:justify-center">
                             <p className="text-4xl lg:text-base font-semibold text-gray-800">Technoligies:</p>
                             <p className="text-4xl lg:text-base text-gray-800 pl-5">Three.js, React.js</p>
